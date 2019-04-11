9fc24274b42182f3e4db8fdf0fa8664847fd664a
Mario Michel <1108mario@gmail.com>
2019-04-11 11:14:49 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Phase 1 Standart Hosts to check
 echo google.at >> hosts.txt
 echo 1.1.1.1 >> hosts.txt
+echo "Standart Hosts to check has been set"
 
-echo "Seting up Systemd Service"
+# Phase 2 Seting up Systemd Service
 echo "[Unit]" >> /etc/systemd/system/margios.service
 echo "Description=Marios's Nagius" >> /etc/systemd/system/margios.service
 echo "After=network-online.target" >> /etc/systemd/system/margios.service
@@ -16,11 +18,16 @@ echo "ExecStart=/home/pi/scripts/margios.sh" >> /etc/systemd/system/margios.serv
 echo "" >> /etc/systemd/system/margios.service
 echo "[Install]" >> /etc/systemd/system/margios.service
 echo "WantedBy=multi-user.target" >> /etc/systemd/system/margios.service
+echo "Systemd Service was created"
 
+# Phase 3 Enable Autostartup
 systemctl enable margios.service
 systemctl start margios.service
 systemctl status margios.service
+echo "Autostartup was enabled"
 
+# Phase 4 Install telegram-send 
 sudo apt-get install python3-pip
 sudo pip3 install telegram-send
 telegram-send --configure
+echo "Telegram send has been configured"
